664962b4f2aae621fdf1cae6e5e035174474e9b0
wanjiban <109531247+wanjiban@users.noreply.github.com>
2026-03-23 08:41:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -41,8 +41,8 @@ const directDomains = [
 ];
 
 // 默认优选IP来源URL
-const defaultIPURL = 'https://raw.githubusercontent.com/qwer-search/bestip/refs/heads/main/kejilandbestip.txt';
-//const defaultIPURL = 'https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestCF/bestcfv6.txt';
+//const defaultIPURL = 'https://raw.githubusercontent.com/qwer-search/bestip/refs/heads/main/kejilandbestip.txt';
+const defaultIPURL = 'https://cfipdb.pages.dev/all';
 //const defaultIPURL = 'https://github.com/qwer-search/bestip/blob/main/addressesapi.txt';
 // UUID验证
 function isValidUUID(str) {
